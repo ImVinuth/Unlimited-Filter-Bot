8f917d39de46dd41679637f001d6114f56e32cd1
user name <91249335+ImVinuth@users.noreply.github.com>
2022-01-05 18:19:18 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Developer", url="https://t.me/VinuthPMBot"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Updates Channel", url="https://t.me/Vinuth_BOTs"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/Vinuth690")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "Updates Channel", url="https://t.me/Vinuth_BOTs")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
